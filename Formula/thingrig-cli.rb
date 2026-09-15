47ec8948341ccfee5ba8dbf0d0551abc74ea4739
thingrigbot <engineering@thingrig.com>
2026-09-15 16:05:15 +0000

    Update thingrig-cli to v1.4.0

--- Formula/thingrig-cli.rb
+++ Formula/thingrig-cli.rb
@@ -1,30 +1,30 @@
 class ThingrigCli < Formula
   desc "Terminal user interface for managing ThingRig devices and remote sessions"
   homepage "https://thingrig.com"
-  version "1.3.0"
+  version "1.4.0"
   license "LicenseRef-ThingRig"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.3.0/thingrig-cli_1.3.0_darwin_arm64.tar.gz"
-      sha256 "f35d04b9a89ae6b37d8fe286e4ffad1807d3ce443eb042df4ca4ada731d63c17"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_darwin_arm64.tar.gz"
+      sha256 "308c6d133a377ffa5858ff7a4c19398284bc3d4edddffe648a8ced7b4c48413f"
     else
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.3.0/thingrig-cli_1.3.0_darwin_amd64.tar.gz"
-      sha256 "0bf520f097c1290977d30afb0c67559980089166e8b4ee058997e3715e5eadfd"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_darwin_amd64.tar.gz"
+      sha256 "65fd212d7c62d963bce7b11c3b1e928ccce5d5fff82190895b2806814c47cfaf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.3.0/thingrig-cli_1.3.0_linux_arm64.tar.gz"
-      sha256 "63d427f4b35723bff5f403d14b108e850647844733ca5be6db66a4e7e8b41224"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_linux_arm64.tar.gz"
+      sha256 "db3b3e65a0d634a1fc75ecbdc972ad330ee2c6aa69249995b672fb79ae5a6e61"
     else
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.3.0/thingrig-cli_1.3.0_linux_amd64.tar.gz"
-      sha256 "f2bcb0bf85e6e43ad309c29b0ee81a2e868248249da5fefaf4d96fb6701d9608"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_linux_amd64.tar.gz"
+      sha256 "9e54b2e149170b315dee709f4715ab6dd142c0330224657b8d76df489ebd635f"
     end
   end
 
   def install
-    bin.install "thingrig-cli"
+    bin.install "thingrig"
   end
 end
