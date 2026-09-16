271d4a24147f715ef096091830c485ae28d5aef2
thingrigbot <engineering@thingrig.com>
2026-09-16 15:40:03 +0000

    Update thingrig-cli to v1.5.0

--- Formula/thingrig-cli.rb
+++ Formula/thingrig-cli.rb
@@ -1,26 +1,26 @@
 class ThingrigCli < Formula
   desc "Terminal user interface for managing ThingRig devices and remote sessions"
   homepage "https://thingrig.com"
-  version "1.4.0"
+  version "1.5.0"
   license "LicenseRef-ThingRig"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_darwin_arm64.tar.gz"
-      sha256 "308c6d133a377ffa5858ff7a4c19398284bc3d4edddffe648a8ced7b4c48413f"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.5.0/thingrig-cli_1.5.0_darwin_arm64.tar.gz"
+      sha256 "a81fc6b8d9d997062e77cff44657d056f3b4bc308c01469690cd339057cb3820"
     else
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_darwin_amd64.tar.gz"
-      sha256 "65fd212d7c62d963bce7b11c3b1e928ccce5d5fff82190895b2806814c47cfaf"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.5.0/thingrig-cli_1.5.0_darwin_amd64.tar.gz"
+      sha256 "cdcf8f746f51f6ad45eda8bcf1cdd4b2e5ebc2228f30cad3ba59a7ef6720055e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_linux_arm64.tar.gz"
-      sha256 "db3b3e65a0d634a1fc75ecbdc972ad330ee2c6aa69249995b672fb79ae5a6e61"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.5.0/thingrig-cli_1.5.0_linux_arm64.tar.gz"
+      sha256 "9741dcd453ec2136850fa00fb1451a6eb8ddf6f4021751e17ca6578debd4447e"
     else
-      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.4.0/thingrig-cli_1.4.0_linux_amd64.tar.gz"
-      sha256 "9e54b2e149170b315dee709f4715ab6dd142c0330224657b8d76df489ebd635f"
+      url "https://github.com/thingrig/thingrig-cli/releases/download/v1.5.0/thingrig-cli_1.5.0_linux_amd64.tar.gz"
+      sha256 "e97aba38f2fd7bca5e1087c475b81f13f8e56307124a20d878d780d2168e749f"
     end
   end
 
